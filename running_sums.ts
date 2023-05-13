cb6ce5b3f726b3ee68bdc21b5238752c020b0b69
Abbas <muhdabbas98@gmail.com>
2023-05-13 22:11:53 +0800

    Better solution

--- running_sums.ts
+++ running_sums.ts
@@ -12,4 +12,22 @@ function runningSum(nums: number[]): number[] {
   return running_sums
 }
 
+function runningSumsMap(nums: number[]) {
+  let result: number = 0
+  return nums.map(item => (result += item))
+}
+
+function runningSum3(nums: number[]): number[] {
+  let res: number[] = []
+  if (nums.length == 0) return res
+  res[0] = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    res[i] = res[i - 1] + nums[i]
+  }
+
+  return res
+}
+
 runTest(runningSum, 'forLoop', [1, 2, 3, 4])
+runTest(runningSumsMap, 'hashMap', [1, 2, 3, 4])
+runTest(runningSum3, 'forLoop', [1, 2, 3, 4])
